d143fea80de9f73b2e1eca924886981f778d7f89
laura krouse <laura.krouse@pcc.edu>
2016-10-18 15:49:29 -0700

    add new task. Add new component. Add new task from child. Add link tags in entry point

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,49 +1,38 @@
 import { Component } from '@angular/core';
+import { Task } from './task.model';
 
 @Component({
   selector: 'my-app',
   template: `
   <div class="container">
     <h1>My First Angular 2 App</h1>
-    <div *ngFor="let currentTask of tasks">
-       <h3>{{ currentTask.description }}</h3>
-  <button (click)="showDetails(currentTask)"> Edit</button>
-  <div>
-  <div *ngIf="selectedTask">
-  <h3> Edit Task </h3>
-  <div>
-  <label> Enter Task Description</label>
-  <input [(ngModel)]="selectedTask.description">
+    <pies></pies>
+    <task-list
+      [childTaskList]="masterTaskList"
+      (clickSender)="showDetails($event)"></task-list>
+   <edit-task
+      [childSelectedTask]="selectedTask"
+      (doneClickedSender)="finishedEditing()"></edit-task>
+   <new-task (newTaskSender)="addTask($event)"></new-task>
   </div>
-  <div>
-  <label> Enter Task ID</label>
-  <input [(ngModel)]="selectedTask.id">
-  <button (click)="finishedEditing()">Done</button>
-  </div>
-  </div>
-  </div>
-
   `
 })
 
 export class AppComponent {
-  public tasks: Task[] = [
+  public masterTaskList: Task[] = [
     new Task("Pass this class", 0),
     new Task("Get Mexican Food", 1),
     new Task("Hike Timberline Trail", 2),
     new Task("Go for a run", 3)
   ];
   selectedTask: Task = null;
-    showDetails(clickedTask: Task) {
-      this.selectedTask = clickedTask;
+  showDetails(clickedTask: Task) {
+    this.selectedTask = clickedTask;
   }
     finishedEditing() {
       this.selectedTask = null;
     }
-}
-
-
-export class Task {
-  public done: boolean = false;
-  constructor(public description: string, public id: number) {   }
+    addTask(newTaskFromChild: Task) {
+      this.masterTaskList.push(newTaskFromChild);
+    }
 }
